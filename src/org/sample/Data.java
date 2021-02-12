c90f52ce0f8a8fe2e4029afbb18fe58bad03e175
Ramkumar090986 <ramkumar8687@gmail.com>
2021-02-12 19:24:32 +0530

    First GIT

---
+++ src/org/sample/Data.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Data {
+	
+	public void print() {
+		System.out.println("Print");
+
+	}
+	public static void main(String[] args) {
+		Data d=new Data();
+		d.print();
+	}
+
+}
